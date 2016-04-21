ad2dd2dfed8148c32e102b1d3f48dcbab540ed72
Niko <kolu4ka02031995@gmail.com>
2016-04-21 17:50:39 +0300

    fix carousel responsive

--- views/home/index.php
+++ views/home/index.php
@@ -1,11 +1,18 @@
 <?php
 use yii\helpers\Html;
-use yii\bootstrap\Carousel;
 use yii\bootstrap\Nav;
 use app\assets\AppAsset;
-
+use Yii\base;
+use app\models\OutputImages;
 
 AppAsset::register($this);
+
+$path = Yii::$app->request->baseUrl;
+$dir = $_SERVER['DOCUMENT_ROOT'].$path.'/images/carousel';
+$dirLocal = $path.'/images/carousel/';
+$imageObject = new OutputImages();
+$all_pictures = $imageObject->outDirFile($dir);
+
 ?>
 <?php $this->beginPage() ?>
     <!DOCTYPE html>
@@ -19,76 +26,76 @@ AppAsset::register($this);
     </head>
     <body>
     <?php $this->beginBody() ?>
-    <div class="layer1">
-        <?php
-        echo Nav::widget([
-            'items' => [
-                [
-                    'label' => 'Results',
-                    'url' => ['home/results'],
-                    'visible' => Yii::$app->user->isGuest
-                ],
-                [
-                    'label' => 'Sport news',
-                    'url' => ['home/sport-news'],
-                    'visible' => Yii::$app->user->isGuest
-                ],
-                [
-                    'label' => 'What is betting',
-                    'url' => ['home/what-is-betting'],
-                    'visible' => Yii::$app->user->isGuest
-                ],
-                [
-                    'label' => 'Types of bets',
-                    'url' => ['home/types-of-bets'],
-                    'visible' => Yii::$app->user->isGuest
-                ],
-                [
-                    'label' => 'EURO 2016',
-                    'url' => ['home/euro-2016'],
-                    'visible' => Yii::$app->user->isGuest
-                ],
-                [
-                    'label' => 'WORLD CUP 2018',
-                    'url' => ['home/world-cup-2018'],
-                    'visible' => Yii::$app->user->isGuest
-                ],
-            ],
-            'options' => ['class' => ' nav nav-pills nav-stacked'],
-        ]);
-        ?>
-    </div>
-    <div class="container" align="center" data-ride="carousel" style="height: 300px;width: 500px">
-        <div id="carousel" class="carousel slide" data-ride="carousel">
-            <!-- Indicators -->
-            <ol class="carousel-indicators">
-                <li data-target="#carousel" data-slide-to="0" class="active"></li>
-                <li data-target="#carousel" data-slide-to="1"></li>
-                <li data-target="#carousel" data-slide-to="2"></li>
-            </ol>
-
-            <!-- Wrapper for slides -->
-            <div class="carousel-inner" role="listbox">
-                <div class="item active">
-                    <img style="width: 500px;height: 300px" src="../web/css/images/1.jpg" alt="">
-                </div>
-                <div class="item">
-                    <img style="width: 500px;height: 300px" src="../web/css/images/2.jpg" alt="">
-                </div>
-                <div class="item">
-                    <img style="width: 500px;height: 300px" src="../web/css/images/3.jpg" alt="">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-2 col-md-2 col-sm-8 col-xs-12">
+                <?php
+                echo Nav::widget([
+                    'items' => [
+                        [
+                            'label' => 'Results',
+                            'url' => ['home/results'],
+                            'visible' => Yii::$app->user->isGuest
+                        ],
+                        [
+                            'label' => 'Sport news',
+                            'url' => ['home/sport-news'],
+                            'visible' => Yii::$app->user->isGuest
+                        ],
+                        [
+                            'label' => 'What is betting',
+                            'url' => ['home/what-is-betting'],
+                            'visible' => Yii::$app->user->isGuest
+                        ],
+                        [
+                            'label' => 'Types of bets',
+                            'url' => ['home/types-of-bets'],
+                            'visible' => Yii::$app->user->isGuest
+                        ],
+                        [
+                            'label' => 'EURO 2016',
+                            'url' => ['home/euro-2016'],
+                            'visible' => Yii::$app->user->isGuest
+                        ],
+                        [
+                            'label' => 'WORLD CUP 2018',
+                            'url' => ['home/world-cup-2018'],
+                            'visible' => Yii::$app->user->isGuest
+                        ],
+                    ],
+                    'options' => ['class' => ' nav nav-pills nav-stacked'],
+                ]);
+                ?>
+            </div>
+            <div class="col-lg-10 col-md-10 col-sm-8 col-xs-12 carousel-size">
+                <div id="carousel" class="carousel slide" data-ride="carousel">
+                    <ol class="carousel-indicators">
+                        <li data-target="#carousel" data-slide-to="0" class="active"></li>
+                        <?php for($i = 1 ; $i < count($all_pictures); $i++) { ?>
+                            <li data-target="#carousel" data-slide-to="<?=$i?>"></li>
+                        <?php } ?>
+                    </ol>
+                    <div class="carousel-inner" role="listbox">
+                        <div class="item active">
+                            <img style="width: 500px;height: 300px" src="<?=$dirLocal.$all_pictures[0]?>" alt="">
+                        </div>
+                        <?php array_shift($all_pictures); ?>
+                        <?php foreach($all_pictures as $picture) { ?>
+                            <div class="item">
+                                <img style="width: 500px;height: 300px" src="<?=$dirLocal.$picture?>" alt="">
+                            </div>
+                        <?php } ?>
+                    </div>
+                    <a class="left carousel-control" href="#carousel" role="button" data-slide="prev">
+                        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="right carousel-control" href="#carousel" role="button" data-slide="next">
+                        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                    </a>
                 </div>
             </div>
-
-            <!-- Controls -->
-            <a class="left carousel-control" href="#carousel" role="button" data-slide="prev">
-                <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-                <span class="sr-only">Previous</span>
-            </a>
-            <a class="right carousel-control" href="#carousel" role="button" data-slide="next">
-                <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-                <span class="sr-only">Next</span>
-            </a>
         </div>
     </div>
     <?php $this->endBody() ?>
